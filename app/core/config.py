1af23f1f9db89451027ef9f01fbbe063eaecfd3c
Artem Larionenko <74620760+lrrrtm@users.noreply.github.com>
2025-11-24 11:20:08 +0000

    added bearer token to auth

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql+asyncpg://postgres:password@localhost:5432/avito_pr_service"
+    API_TOKEN: str = "avito"
 
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from sqlalchemy import text
 from app.api.v1.endpoints import teams, users, pull_requests, stats, health
 from app.db.session import engine
 from app.db.base import Base
 from app.models import team, user, pull_request
+from app.core.security import verify_token
 
 
 @asynccontextmanager
@@ -18,8 +19,8 @@ app = FastAPI(
     title="PR Reviewer Assignment Service (Test Task, Fall 2025, by lrrrtm)", version="1.0.0", lifespan=lifespan
 )
 
-app.include_router(teams.router, prefix="/team", tags=["Teams"])
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(pull_requests.router, prefix="/pullRequest", tags=["PullRequests"])
-app.include_router(stats.router, prefix="/stats", tags=["Stats"])
+app.include_router(teams.router, prefix="/team", tags=["Teams"], dependencies=[Depends(verify_token)])
+app.include_router(users.router, prefix="/users", tags=["Users"], dependencies=[Depends(verify_token)])
+app.include_router(pull_requests.router, prefix="/pullRequest", tags=["PullRequests"], dependencies=[Depends(verify_token)])
+app.include_router(stats.router, prefix="/stats", tags=["Stats"], dependencies=[Depends(verify_token)])
 app.include_router(health.router, prefix="/health", tags=["Health"])
